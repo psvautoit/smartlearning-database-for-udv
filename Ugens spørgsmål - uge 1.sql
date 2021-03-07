9a4c75d53da9bf794356cac0330f28a22e9c757f
Philip Holm Svendsen <59566522+psvautoit@users.noreply.github.com>
2021-03-07 20:59:21 +0100

    psv

---
+++ Ugens spørgsmål - uge 1.sql
@@ -0,0 +1,21 @@
+-- Ugens spørgsmål
+
+SELECT count(*)
+FROM AdventureWorks2019.Person.Person
+
+
+SELECT PostalCode, City,  *
+FROM Person.Address
+
+SELECT [AddressID]
+      ,[AddressLine1]
+      ,[AddressLine2]
+      ,[City]
+      ,[StateProvinceID]
+      ,[PostalCode]
+      ,[SpatialLocation]
+      ,[rowguid]
+      ,[ModifiedDate]
+FROM [AdventureWorks2019].[Person].[Address]
+
+
