78a9abbe5185cb46dc5ff7fdaa9d8534ed6f5be8
Philip Holm Svendsen <59566522+psvautoit@users.noreply.github.com>
2021-03-21 20:30:17 +0100

    psv

--- temp/SQLQuery_1.sql
+++ temp/SQLQuery_1.sql
@@ -1,2 +1,27 @@
-SELECT COUNT(*)
-FROM [AdventureWorks2019].[Person].[PersonPhone]
+-- Create a new stored procedure called 'GetBooks' in schema 'Fanzy'
+-- Drop the stored procedure if it already exists
+IF EXISTS (
+SELECT *
+    FROM INFORMATION_SCHEMA.ROUTINES
+WHERE SPECIFIC_SCHEMA = N'Fanzy'
+    AND SPECIFIC_NAME = N'GetBooks'
+    AND ROUTINE_TYPE = N'PROCEDURE'
+)
+DROP PROCEDURE Fanzy.GetBooks
+GO
+-- Create the stored procedure in the specified schema
+
+CREATE PROCEDURE Fanzy.GetBooks
+    @BookISBN BIGINT
+
+AS
+
+BEGIN
+    SELECT *
+    FROM Book
+    WHERE ISNULL(@BookISBN, ISBN) = ISBN
+
+END
+
+
+
